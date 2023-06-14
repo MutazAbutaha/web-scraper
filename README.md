031a11479bd5ba9de7edc19debc1cc42fa08d4bb
mutazabutaha <mutazabutaha13@outlook.com>
2023-06-14 22:51:55 +0000

    2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Project Name : web-scarper
 
+# Lab 17
+
 ## Authors : Al Moutaz AbuTaha
+
+## RUN Project : python3 scraper.py
--- scraper.py
+++ scraper.py
@@ -1,17 +1,30 @@
 import requests
 from  bs4 import BeautifulSoup
-import json
+link_of_page = 'https://en.wikipedia.org/wiki/Olive_oil'
 
-URL = 'https://en.wikipedia.org/wiki/Washington,_D.C.'
-def  get_citations_needed_count (URL):
-    page = requests.get(URL)
-    # print(page.content)
-    soup = BeautifulSoup(page.content,'html.parser')
-    # print(soup)
-    all_posts = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")
-    # print(all_posts)
-    citations_needed_count = 0
-    for post in all_posts:
-        citations_needed_count += 1
-    return citations_needed_count
-print("Number Of citations needed is ",get_citations_needed_count(URL))
+def get_counter(link_of_page):
+    page = requests.get(link_of_page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    all_sup = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")
+    counter = 0
+    for post in all_sup:
+        counter += 1
+    print(f" {counter}  Number of Citations needed")
+    return counter
+
+get_counter(link_of_page)
+
+def get_citations_needed_report(link_of_page):
+    page = requests.get(link_of_page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    all_div = soup.find('div', class_="mw-body-content mw-content-ltr")
+    all_p = all_div.find_all("p")
+    list = []
+    for paragraph in all_p:
+        citation = paragraph.find_all("sup", class_="noprint Inline-Template Template-Fact")
+        if citation:
+            list.append(paragraph.text)
+    for p in list:
+        print(f"\n {p} \n")
+
+get_citations_needed_report(link_of_page)   
